60b946bad79b1318f1d3e196f7c14881dc1a4dde
zedongli <928364047@qq.com>
2017-03-23 10:56:18 +0800

    dksflsdl

--- src/ol/dao/ICoureseDao.java
+++ src/ol/dao/ICoureseDao.java
@@ -2,7 +2,14 @@ package ol.dao;
 
 import java.util.List;
 
+
+
+
+
+import java.util.Map;
+
 import ol.entity.Courese;
+import ol.entity.LeanQueryModel;
 
 /**
  * 课程数据库操作
@@ -19,13 +26,30 @@ public interface ICoureseDao {
 	
 	Courese findCourese(int pid);
 	
-	List<Courese> findAllCoureses(int start, int end);
+	List<Courese> findAllCoureses(Integer userid);
 	
-	Integer findAllCouresesCount();
+	Integer findAllCouresesCount(Integer userid);
+	List<Courese> findAllCoureses(Integer userid);
+	
+	Integer findAllCouresesCount(Integer userid);
 	
 	List<Courese> findAllCouresesByModel(String cname, int type, int start, int end);
 	
 	Integer findAllCouresesCountByModel(String cname, int type, int start, int end);
 	
 	List<Courese> findHotCoureses();
+	/**
+	 * 按条件返回
+	 * @param page
+	 * @return
+	 */
+	public List<Courese> searchCourese(LeanQueryModel condition);
+	/**
+	 * 按条件返回总条数
+	 * @param page
+	 * @return
+	 */
+	public Long findCount(LeanQueryModel condition);
+	public List<Courese> findAll();
+	
 }
